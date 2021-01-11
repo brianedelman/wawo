b45e018c5a5cfda0ab621c64620a23b5f551147d
Brian Edelman <bd.edelman@gmail.com>
2021-01-11 00:15:41 -0500

    wip footer and theme updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,25 +1,125 @@
-import { Paper, Typography } from '@material-ui/core';
-import PropTypes from 'prop-types';
+import {
+  Container,
+  Grid,
+  Box,
+  List,
+  ListItem,
+  Hidden,
+  Paper,
+  Typography,
+  IconButton,
+} from '@material-ui/core';
+import Feed from 'react-instagram-authless-feed';
+
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import { makeStyles } from '@material-ui/core/styles';
+
+import ErrorBoundary from 'components/ErrorBoundary';
+import Link from 'components/router/Link';
+import { URLS } from 'constants.js';
+import FooterLogo from 'components/svgs/FooterLogo';
 
 const thisyear = new Date().getFullYear();
-const Footer = ({ className }) => {
+
+const useStyles = makeStyles(theme => ({
+  footer: {
+    backgroundColor: theme.palette.secondary.main,
+    marginTop: 'auto',
+    paddingTop: theme.spacing(4),
+  },
+  links: {
+    '& a': {
+      fontSize: '1rem',
+      color: theme.palette.text.primary,
+    },
+  },
+}));
+
+const Footer = () => {
+  const classes = useStyles();
   return (
-    <Paper className={className} elevation={0}>
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        {thisyear}
-        {'.'}
-      </Typography>
+    <Paper className={classes.footer} elevation={0}>
+      <Container maxWidth="lg" className={classes.height}>
+        <Grid
+          container
+          justify="space-between"
+          alignItems="center"
+          className={classes.height}
+        >
+          <Grid item xs={12} md={6} className={classes.height} />
+          <Hidden smDown implementation="css">
+            <Grid item md={2} className={classes.height} />
+          </Hidden>
+
+          <Grid item xs={12} md={4}>
+            <FooterLogo />
+            <List
+              className={classes.links}
+              component="nav"
+              aria-label="Footer links"
+            >
+              <ListItem disableGutters>
+                <Link href={URLS.contact}>Contact Us</Link>
+              </ListItem>
+
+              <ListItem disableGutters>
+                <Link href={URLS.conditions}>Terms and Conditions</Link>
+              </ListItem>
+
+              <ListItem disableGutters>
+                <Link href={URLS.termsUse}>Terms of Use</Link>
+              </ListItem>
+
+              <ListItem disableGutters>
+                <Link href={URLS.privacy}>Privacy Policy</Link>
+              </ListItem>
+
+              <ListItem disableGutters>
+                <IconButton
+                  href="https://www.facebook.com/wearewomenowned/"
+                  target="_blank"
+                  rel="noopener nofollow"
+                >
+                  <FacebookIcon />
+                </IconButton>
+                <IconButton
+                  href="https://www.instagram.com/wearewomenowned/"
+                  target="_blank"
+                  rel="noopener nofollow"
+                >
+                  <InstagramIcon />
+                </IconButton>
+                <IconButton
+                  href="mailto:hello@wearewomenowned.com"
+                  target="_blank"
+                >
+                  <MailOutlineIcon />
+                </IconButton>
+              </ListItem>
+            </List>
+          </Grid>
+        </Grid>
+        <ErrorBoundary>
+          <Feed
+            userName="wearewomenowned"
+            className="Feed"
+            classNameLoading="Loading"
+            limit="3"
+          />
+        </ErrorBoundary>
+
+        <Box my={2}>
+          <Typography variant="body1" color="textSecondary" align="center">
+            {'Copyright © '}
+            {thisyear}
+            {'.'}
+          </Typography>
+        </Box>
+      </Container>
     </Paper>
   );
-};
-
-Footer.propTypes = {
-  className: PropTypes.string,
-};
-
-Footer.defaultProps = {
-  className: '',
 };
 
 export default Footer;
--- src/theme/theme.js
+++ src/theme/theme.js
@@ -17,6 +17,62 @@ const theme = createMuiTheme({
       default: '#fff',
     },
     black: '#222',
+    salmon: '#FA9987',
+    text: {
+      primary: '#222',
+      secondary: '#293039',
+    },
+  },
+  typography: {
+    fontFamily: '"Poppins", sans-serif',
+    h1: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '3rem', // 48px
+      '@media (min-width:600px)': {
+        fontSize: '2.25rem', // 36px
+      },
+    },
+    h2: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '2.5rem', // 40px
+      '@media (min-width:600px)': {
+        fontSize: '1.75rem', // 28px
+      },
+    },
+    h3: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '2.25rem', // 36px
+      '@media (min-width:600px)': {
+        fontSize: '1.5rem', // 24px
+      },
+    },
+    h4: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '1.250rem', // 20px
+      '@media (min-width:600px)': {
+        fontSize: '1.25rem', // 18px
+      },
+    },
+    h5: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '1.250rem', // 20px
+      '@media (min-width:600px)': {
+        fontSize: '1.063rem', // 17px
+      },
+    },
+    h6: {
+      fontFamily: '"Mermaid", serif',
+      fontWeight: 700,
+      fontSize: '1rem',
+      '@media (min-width:600px)': {
+        fontSize: '1rem',
+      },
+    },
   },
 });
 
